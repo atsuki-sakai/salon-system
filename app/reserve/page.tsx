f5e7816031adcb426f27ac106d1a226beab2df80
Atsuki Sakai <atk721@icloud.com>
2025-03-05 00:37:11 +0900

    feat: 予約機能の実装

---
+++ app/reserve/page.tsx
@@ -0,0 +1,14 @@
+export default function ReservePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen max-w-5xl mx-auto">
+      <div className="flex flex-col items-start justify-center">
+        <h1 className="text-3xl font-bold ">500: Error</h1>
+        <p className="text-sm text-gray-500">
+          予約ページにアクセスできません。
+          <br />
+          再度サロンの予約ページからアクセスしてください。
+        </p>
+      </div>
+    </div>
+  );
+}
