44a0f4729bc92d2816c463e5578500ae89c36d38
Atsuki Sakai <atk721@icloud.com>
2025-03-04 17:08:39 +0900

    feat: update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 
@@ -26,6 +27,12 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
+          <Toaster
+            position="top-right"
+            expand={false}
+            richColors
+            duration={5000}
+          />
         </body>
       </ClerkProvider>
     </html>
