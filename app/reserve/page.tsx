1d3df6ff3ee13c73d8e143d11cf2f38bc5c52829
Atsuki Sakai <atk721@icloud.com>
2025-03-05 16:48:26 +0900

    feat: 予約トップページの実装

--- app/reserve/page.tsx
+++ app/reserve/page.tsx
@@ -1,14 +1,27 @@
-export default function ReservePage() {
+// app/reserve/page.tsx
+"use client";
+
+// import { toast } from "sonner";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import Link from "next/link";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function ReserveRedirectPage() {
+  const salons = useQuery(api.users.getPaginatedUsers, {
+    limit: 10,
+  });
+  // ロード中でない場合のフォールバック表示
   return (
-    <div className="flex flex-col items-center justify-center h-screen max-w-5xl mx-auto">
-      <div className="flex flex-col items-start justify-center">
-        <h1 className="text-3xl font-bold ">500: Error</h1>
-        <p className="text-sm text-gray-500">
-          予約ページにアクセスできません。
-          <br />
-          再度サロンの予約ページからアクセスしてください。
-        </p>
-      </div>
+    <div className="flex flex-col items-center justify-center h-screen max-w-5xl mx-auto space-y-4">
+      {salons?.users.map((salon) => (
+        <Card key={salon._id}>
+          <CardHeader>
+            <CardTitle>{salon.email}</CardTitle>
+            <Link href={`/reserve/${salon.clerkId}`}>予約する</Link>
+          </CardHeader>
+        </Card>
+      ))}
     </div>
   );
 }
