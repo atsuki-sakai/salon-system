6608b5f3c21ddad6b80d8f35109dd142cae70bc1
Atsuki Sakai <atk721@icloud.com>
2025-03-10 02:11:12 +0900

    feat: add authentication configuration

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://daring-bison-43.clerk.accounts.dev/",
+      applicationID: "convex", // audフィールド値
+    },
+  ],
+};
+
+export default authConfig;
+  
