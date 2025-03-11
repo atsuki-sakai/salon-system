29b8936a2bdfdc036492b90d6b6a82a32b024fba
Atsuki Sakai <atk721@icloud.com>
2025-03-11 23:35:29 +0900

    予約ページの更新

--- app/reservation/page.tsx
+++ app/reservation/page.tsx
@@ -7,16 +7,29 @@ import { LINE_LOGIN_SESSION_KEY } from "@/lib/constants";
 import { getCookie } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 export default function ReserveRedirectPage() {
+  console.log("liff");
+
   const { liff } = useLiff();
   const router = useRouter();
   useEffect(() => {
-    const session = getCookie(LINE_LOGIN_SESSION_KEY);
-    if (session) {
-      const parsedSession = JSON.parse(session);
-      console.log("parsedSession", parsedSession);
-      router.push(`/reservation/${parsedSession.storeId}/calendar`);
+    if (liff?.isLoggedIn()) {
+      const session = getCookie(LINE_LOGIN_SESSION_KEY);
+      if (session) {
+        const { storeId } = JSON.parse(session);
+        if (storeId) {
+          const redirectUrl = `/reservation/${storeId}/calendar`;
+          router.push(redirectUrl);
+        } else {
+          console.log("storeId is not found");
+          router.push("/");
+        }
+      } else {
+        console.log("session is not found");
+        router.push("/");
+      }
     }
   }, [liff, router]);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <span className="text-2xl font-bold">Redirecting...</span>
